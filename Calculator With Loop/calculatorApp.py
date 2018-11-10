50c870f65e4ae9fa20c42a317787e426ebfc0e5f
Sana <mail.sanadiwan@gmail.com>
2018-11-10 14:49:17 +0000

    Calculator with loop added

---
+++ Calculator With Loop/calculatorApp.py
@@ -0,0 +1,19 @@
+calculate="Yes"
+while(calculate=="Yes"):
+    num1 = input("Please enter the first number ")
+    num2 = input("Please enter the second number ")
+    print("What would you like to do with "+str(num1)+" and "+str(num2)+" ?")
+    action = raw_input("(Hint: Type 'multiply', 'add', 'divide' or 'subtract'!) ")
+
+    if(action=="add"):
+        print("The sum of "+str(num1)+" and "+str(num2)+" is: "+str(num1+num2))
+    elif(action == "subtract"):
+        print("The difference of "+str(num1)+" and "+str(num2)+" is: "+str(num1-num2))
+    elif(action == "multiply"):
+        print(str(num1)+" multiplied by "+str(num2)+" is: "+str(num1*num2))
+    elif(action == "divide"):
+        print(str(num1)+" divided by "+str(num2)+" is: "+str(num1/num2))
+    else:
+        action = input("Error: Calculator failed to understand the action. \n Try typing 'add','subtract','multiply' or 'divide' ")
+    calculate = raw_input("Would you like to calculate some more? ")
+print("Calculator exited")
